aa2e6f17a05814eca1364e5103d66111244d7862
gustavo-jess <160682673+gustavo-jess@users.noreply.github.com>
2024-08-14 22:01:58 -0300

    Add files via upload

---
+++ AtvUmManipulacaoVariaveis/src/Main.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+
+    var altura: Double = 1.8
+    var idade: Int = 19
+    var idade2: Int = 20
+    var nome: String = "jess"
+    var ePessoa: Boolean = true
+    var somaidade: Int = idade + idade2
+    var somaaltura: Double = altura *2
+
+    println("ola $nome!" )
+    println(" A altura é $altura!")
+    println("A idade dois é $idade!")
+    println("A idade dois é $idade2")
+    println("o nome é $nome")
+    println("você é um estudante? $ePessoa")
+    println("A soma das idades é $somaidade")
+    println("A multiplicação da altura é $somaaltura")
+
+
+
+}
